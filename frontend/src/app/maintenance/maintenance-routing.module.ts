928de8f2d2bf37c3d9ceeb83f9a8608a95196401
Hubeau Lionel <lionel.hubeau@smals.be>
2022-06-21 12:15:22 +0200

    Fix build error

--- frontend/src/app/maintenance/maintenance-routing.module.ts
+++ frontend/src/app/maintenance/maintenance-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContractManagementComponent } from './pages/contract-management/contract-management.component';
+import { DatabaseCountersComponent } from './pages/database-counters/database-counters.component';
 
 const routes: Routes = [
-  { path: '', component: ContractManagementComponent }
+  { path: '', component: DatabaseCountersComponent }
 ];
 
 @NgModule({
