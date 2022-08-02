ea8b522d0f5f03d07a1958463be2e784d91f6a9b
Hubeau Lionel <lionel.hubeau@gmail.com>
2022-08-02 18:24:00 +0200

    Fix flyway migration

--- backend/src/main/resources/db/migrations/V2022080218122__fix_player_draw_error.sql
+++ backend/src/main/resources/db/migrations/V2022080218122__fix_player_draw_error.sql
@@ -1,6 +1,6 @@
-INSERT INTO tbl_player_draws (pld_drw_id, pld_pl_id, pld_draw_score, pld_event_score, pld_rol_id, pld_dealer) VALUES (85, 1, -3, 26, 4, 0);
-INSERT INTO tbl_player_draws (pld_drw_id, pld_pl_id, pld_draw_score, pld_event_score, pld_rol_id, pld_dealer) VALUES (85, 2, -3, -18, 5, 1);
-INSERT INTO tbl_player_draws (pld_drw_id, pld_pl_id, pld_draw_score, pld_event_score, pld_rol_id, pld_dealer) VALUES (85, 5, 9, -12, 1, 0);
-INSERT INTO tbl_player_draws (pld_drw_id, pld_pl_id, pld_draw_score, pld_event_score, pld_rol_id, pld_dealer) VALUES (85, 6, -3, 4, 6, 0);
+INSERT INTO tbl_player_draws (pld_drw_id, pld_pl_id, pld_draw_score, pld_event_score, pld_rol_id, pld_dealer, pld_creation_date, pld_last_update_date) VALUES (85, 1, -3, 26, 4, 0, TIMESTAMP '2022-08-02 18:22:30', TIMESTAMP '2022-08-02 18:22:30');
+INSERT INTO tbl_player_draws (pld_drw_id, pld_pl_id, pld_draw_score, pld_event_score, pld_rol_id, pld_dealer, pld_creation_date, pld_last_update_date) VALUES (85, 2, -3, -18, 5, 1, 0, TIMESTAMP '2022-08-02 18:22:30', TIMESTAMP '2022-08-02 18:22:30');
+INSERT INTO tbl_player_draws (pld_drw_id, pld_pl_id, pld_draw_score, pld_event_score, pld_rol_id, pld_dealer, pld_creation_date, pld_last_update_date) VALUES (85, 5, 9, -12, 1, 0, 0, TIMESTAMP '2022-08-02 18:22:30', TIMESTAMP '2022-08-02 18:22:30');
+INSERT INTO tbl_player_draws (pld_drw_id, pld_pl_id, pld_draw_score, pld_event_score, pld_rol_id, pld_dealer, pld_creation_date, pld_last_update_date) VALUES (85, 6, -3, 4, 6, 0, 0, TIMESTAMP '2022-08-02 18:22:30', TIMESTAMP '2022-08-02 18:22:30');
 
 DELETE FROM tbl_draw WHERE drw_id=86;
