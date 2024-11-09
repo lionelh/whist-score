f9ec2ad74f605d717a11f0df0c49de206964af79
lionelh <lionel.hubeau@gmail.com>
2024-11-09 17:00:15 +0100

    Fix some score inconsistencies

---
+++ backend/src/main/resources/db/migrations/V20241109165100__fix_data_inconsistencies_in_result_score.sql
@@ -0,0 +1,4 @@
+UPDATE tbl_result_roles SET rer_score=0 WHERE rer_rol_id=8 AND rer_res_id IN (107,108);
+UPDATE tbl_result_roles SET rer_score=-4 WHERE rer_rol_id=4 AND rer_res_id IN (107,108);
+UPDATE tbl_result_roles SET rer_score=-4 WHERE rer_rol_id=5 AND rer_res_id IN (107,108);
+UPDATE tbl_result_roles SET rer_score=-4 WHERE rer_rol_id=6 AND rer_res_id IN (107,108);
