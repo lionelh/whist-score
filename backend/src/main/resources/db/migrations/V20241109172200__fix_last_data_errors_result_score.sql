2e344a9b71fe672f774a6a97c6361a52ecd83152
lionelh <lionel.hubeau@gmail.com>
2024-11-09 17:28:04 +0100

    Fix last score inconsistencies

---
+++ backend/src/main/resources/db/migrations/V20241109172200__fix_last_data_errors_result_score.sql
@@ -0,0 +1,5 @@
+UPDATE tbl_result_roles SET rer_score=6 WHERE rer_rol_id=1 AND rer_res_id=109;
+UPDATE tbl_result_roles SET rer_score=-2 WHERE rer_rol_id=4 AND rer_res_id=109;
+UPDATE tbl_result_roles SET rer_score=-2 WHERE rer_rol_id=5 AND rer_res_id=109;
+UPDATE tbl_result_roles SET rer_score=-2 WHERE rer_rol_id=6 AND rer_res_id=109;
+UPDATE tbl_result_roles SET rer_score=0 WHERE rer_rol_id=8 AND rer_res_id=109;
