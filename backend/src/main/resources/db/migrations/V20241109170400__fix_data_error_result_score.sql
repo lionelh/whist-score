4fb78d0f2c2079de037850bccf111e313136e40a
lionelh <lionel.hubeau@gmail.com>
2024-11-09 17:10:44 +0100

    Fix some score inconsistencies

---
+++ backend/src/main/resources/db/migrations/V20241109170400__fix_data_error_result_score.sql
@@ -0,0 +1,11 @@
+UPDATE tbl_result_roles SET rer_score=6 WHERE rer_rol_id=1 AND rer_res_id=107;
+UPDATE tbl_result_roles SET rer_score=-2 WHERE rer_rol_id=4 AND rer_res_id=107;
+UPDATE tbl_result_roles SET rer_score=-2 WHERE rer_rol_id=5 AND rer_res_id=107;
+UPDATE tbl_result_roles SET rer_score=-2 WHERE rer_rol_id=6 AND rer_res_id=107;
+UPDATE tbl_result_roles SET rer_score=0 WHERE rer_rol_id=8 AND rer_res_id=107;
+
+UPDATE tbl_result_roles SET rer_score=9 WHERE rer_rol_id=1 AND rer_res_id=108;
+UPDATE tbl_result_roles SET rer_score=-3 WHERE rer_rol_id=4 AND rer_res_id=108;
+UPDATE tbl_result_roles SET rer_score=-3 WHERE rer_rol_id=5 AND rer_res_id=108;
+UPDATE tbl_result_roles SET rer_score=-3 WHERE rer_rol_id=6 AND rer_res_id=108;
+UPDATE tbl_result_roles SET rer_score=0 WHERE rer_rol_id=8 AND rer_res_id=108;
